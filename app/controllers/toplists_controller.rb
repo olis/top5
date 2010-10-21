8b4bbc53cd6f80615af3bffdaec3faa0e01aa810
Oliver Schemmel <os@yum.de>
2010-10-21 22:28:27 +0200

    new toplist can be created

--- app/controllers/toplists_controller.rb
+++ app/controllers/toplists_controller.rb
@@ -6,6 +6,18 @@ class ToplistsController < ApplicationController
   
   def new
     @toplist = Toplist.new
+    @toplist.build_items
+  end
+  
+  def create
+    @toplist = Toplist.new(params[:toplist])
+    
+    if @toplist.save
+      redirect_to toplists_path
+      #redirect_to user_toplists_path(current_user)
+    else
+      render :new
+    end
   end
   
 end
--- app/models/toplist.rb
+++ app/models/toplist.rb
@@ -1,4 +1,15 @@
 class Toplist < ActiveRecord::Base
   belongs_to :topic
   has_many :items
+  
+  accepts_nested_attributes_for :topic, :reject_if => proc { |attributes| attributes['title'].blank? }
+  accepts_nested_attributes_for :items
+  
+  default_scope :order => 'updated_at desc'
+  
+  def build_items
+    5.times do |i|
+      items.build(:position => i + 1)
+    end
+  end
 end
