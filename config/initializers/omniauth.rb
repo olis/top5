08dbecf92f93d0fdf58e4286c5cf45dbe2ca5a2a
Oliver Schemmel <os@yum.de>
2011-01-13 16:54:45 +0100

    Passwort aus dem Repository entfernt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'abuo9mdiuhUXZTNNtr0Iw', 'PcEcJhJzphcUisyRMJxqWR5ykkC7xpo3HmylShAds'
   #provider :facebook, 'APP_ID', 'APP_SECRET'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
