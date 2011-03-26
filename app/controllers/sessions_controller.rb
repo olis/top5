f8193a80f6ff80e3ff8b673455ac1fe9c9a776f5
Oliver Schemmel <os@yum.de>
2011-03-26 11:26:05 +0100

    Debug-Direktive entfernt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env['rack.auth']
-    debugger
     unless @auth = Authorization.find_from_hash(auth)
       @auth = Authorization.create_from_hash(auth, current_user)
     end
